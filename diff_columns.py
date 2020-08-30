039838bd93a3f5658fa29892d2e305fbf275ad53
Sean Joseph <theseanjoseph@gmail.com>
2020-08-29 22:17:41 -0400

    Applied black formatter

--- diff_columns.py
+++ diff_columns.py
@@ -5,57 +5,72 @@ from pprint import pprint
 import csv
 import sys
 
+
 def get_arguments(argv):
     parser = ArgumentParser(description="Find the differing columns in csv rows")
-    parser.add_argument("-f","--file",help="csv file path",required=True)
-    parser.add_argument("--unique-columns",action="store_true",
-                        help="Make column names unique by combining name and index")
-    parser.add_argument("--include-count",action="store_true",
-                        help="Include counts of the column values")
-    parser.add_argument("--format-csv",action="store_true",
-                        help="Format output in csv")
+    parser.add_argument("-f", "--file", help="csv file path", required=True)
+    parser.add_argument(
+        "--unique-columns",
+        action="store_true",
+        help="Make column names unique by combining name and index",
+    )
+    parser.add_argument(
+        "--include-count",
+        action="store_true",
+        help="Include counts of the column values",
+    )
+    parser.add_argument(
+        "--format-csv", action="store_true", help="Format output in csv"
+    )
     return parser.parse_args(argv[1:])
 
-def get_csv_rows(lines,unique_columns=True):
+
+def get_csv_rows(lines, unique_columns=True):
     """
     Get a list of dicts for each row of the csv
     """
     header, *rows = list(csv.reader(lines))
     # make sure column headers are unique
     if unique_columns:
-        header = [f'{title} - {index}' for index,title in enumerate(header)]
-    return [ dict(zip(header,row)) for row in rows ]
+        header = [f"{title} - {index}" for index, title in enumerate(header)]
+    return [dict(zip(header, row)) for row in rows]
+
 
 def get_csv_columns(rows):
     columns = defaultdict(list)
     for row in rows:
-        for key,value in row.items():
+        for key, value in row.items():
             columns[key].append(value)
     return columns
+
 
 def get_diff_columns(columns):
     def all_same(iterable):
         return len(set(iterable)) == 1
-    return {key: values for key,values in columns.items() if not all_same(values)}
+
+    return {key: values for key, values in columns.items() if not all_same(values)}
+
 
 def add_counts_columns(columns):
-    return {key: dict(Counter(values)) for key,values in columns.items()}
+    return {key: dict(Counter(values)) for key, values in columns.items()}
+
 
 def convert_columns_to_rows(columns):
     header = list(columns.keys())
-    raw_columns = [ columns[key] for key in header ]
-    rows = [ list(row) for row in zip(*raw_columns) ]
+    raw_columns = [columns[key] for key in header]
+    rows = [list(row) for row in zip(*raw_columns)]
     return [header, *rows]
+
 
 def main(argv):
     args = get_arguments(argv)
 
-    with open(args.file,'r') as f:
+    with open(args.file, "r") as f:
         lines = [line.rstrip() for line in f]
 
-    rows = get_csv_rows(lines,args.unique_columns)
+    rows = get_csv_rows(lines, args.unique_columns)
     columns = get_csv_columns(rows)
-    diffs =  get_diff_columns(columns)
+    diffs = get_diff_columns(columns)
     if args.format_csv:
         diff_rows = convert_columns_to_rows(diffs)
         print("\n".join([",".join(row) for row in diff_rows]))
@@ -66,5 +81,6 @@ def main(argv):
         pprint(diffs)
     return 0
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     sys.exit(main(sys.argv))
--- find_super_keys.py
+++ find_super_keys.py
@@ -7,34 +7,54 @@ from argparse import ArgumentParser
 from csv import reader, DictReader
 from re import match
 
+
 def get_arguments(argv):
-    parser = ArgumentParser(description='Find all super keys in a csv file')
-    parser.add_argument('-f','--file',help='CSV file to analyze',required=True)
-    parser.add_argument('-n','--number',help='Number of keys to show',type=int)
-    parser.add_argument('-e','--exclusion',nargs='+',default=[],
-                        help='List of strings to exclude columns from keys')
-    parser.add_argument('-r','--regex-exclusion',help='Regular expression to exclude columns from keys')
-    parser.add_argument('-s','--max-key-size',help='Maximum key size to accept',type=int,default=None)
-    parser.add_argument('--unique-columns',action="store_true",
-                        help='Mutate column name to be "name - index" in order to force uniqueness')
+    parser = ArgumentParser(description="Find all super keys in a csv file")
+    parser.add_argument("-f", "--file", help="CSV file to analyze", required=True)
+    parser.add_argument("-n", "--number", help="Number of keys to show", type=int)
+    parser.add_argument(
+        "-e",
+        "--exclusion",
+        nargs="+",
+        default=[],
+        help="List of strings to exclude columns from keys",
+    )
+    parser.add_argument(
+        "-r",
+        "--regex-exclusion",
+        help="Regular expression to exclude columns from keys",
+    )
+    parser.add_argument(
+        "-s",
+        "--max-key-size",
+        help="Maximum key size to accept",
+        type=int,
+        default=None,
+    )
+    parser.add_argument(
+        "--unique-columns",
+        action="store_true",
+        help='Mutate column name to be "name - index" in order to force uniqueness',
+    )
     return parser.parse_args(argv[1:])
 
-def get_csv_data(file_,force_unique = False):
-    with open(file_,'r') as f:
+
+def get_csv_data(file_, force_unique=False):
+    with open(file_, "r") as f:
         lines = [line.strip() for line in f]
     if force_unique:
         header, *rows = list(reader(lines))
         # make sure column headers are unique
-        header = [f'{title} - {index}' for index,title in enumerate(header)]
-        return (header,[dict(zip(header,row)) for row in rows])
+        header = [f"{title} - {index}" for index, title in enumerate(header)]
+        return (header, [dict(zip(header, row)) for row in rows])
     else:
         dict_reader = DictReader(lines)
-        header=  dict_reader.fieldnames
+        header = dict_reader.fieldnames
         rows = list(dict_reader)
-        return (header,rows)
-        
+        return (header, rows)
 
-def is_super_key(key,rows):
+
+def is_super_key(key, rows):
     values = set()
     for row in rows:
         value = tuple(row[k] for k in key)
@@ -43,27 +63,39 @@ def is_super_key(key,rows):
         values.add(value)
     return True
 
-def filter_regex(header,regex):
+
+def filter_regex(header, regex):
     if regex:
-        header = list(key for key in header if match(regex,key) is None)
+        header = list(key for key in header if match(regex, key) is None)
     return header
 
-def filter_exclusions(header,exclusions):
+
+def filter_exclusions(header, exclusions):
     if exclusions:
         header = [key for key in header if key not in exclusions]
     return header
 
+
 def main(argv):
     args = get_arguments(argv)
-    header,rows = get_csv_data(args.file,args.unique_columns)
-    header = filter_regex(filter_exclusions(header,args.exclusion),args.regex_exclusion)
+    header, rows = get_csv_data(args.file, args.unique_columns)
+    header = filter_regex(
+        filter_exclusions(header, args.exclusion), args.regex_exclusion
+    )
 
-    max_length = args.max_key_size if args.max_key_size and args.max_key_size < len(header) else len(header) 
-    all_combinations = chain.from_iterable(combinations(header,r=i) for i in range(1,max_length+1)) 
-    super_keys = (key for key in all_combinations if is_super_key(key,rows))
-    for key in islice(super_keys,None,args.number):
+    max_length = (
+        args.max_key_size
+        if args.max_key_size and args.max_key_size < len(header)
+        else len(header)
+    )
+    all_combinations = chain.from_iterable(
+        combinations(header, r=i) for i in range(1, max_length + 1)
+    )
+    super_keys = (key for key in all_combinations if is_super_key(key, rows))
+    for key in islice(super_keys, None, args.number):
         print(key)
     return 0
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     sys.exit(main(sys.argv))
